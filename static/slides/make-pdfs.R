2214f5c5ad97276a9f7c9abb3669842eadd74e27
Ryan Safner <ryansafner@gmail.com>
2020-08-28 19:03:48 -0400

    Add 1.5 and 1.6 reading

--- static/slides/make-pdfs.R
+++ static/slides/make-pdfs.R
@@ -1,2 +1,2 @@
 # make-pdfs
-pagedown::chrome_print("https://thoughtf20.classes.ryansafner.com/slides/1.2-slides.html")
+pagedown::chrome_print("https://thoughtf20.classes.ryansafner.com/slides/1.4-slides.html")
