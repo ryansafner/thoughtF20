e2f323b35d5e56419b3e240bf353aa8f0d03888b
Ryan Safner <ryansafner@gmail.com>
2020-10-11 15:25:44 -0400

    Add grade calculator

--- static/thought_grade_calculator/thought_grade_calculator.R
+++ static/thought_grade_calculator/thought_grade_calculator.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("ECON 326 Course Grade Calculator"),
+    titlePanel("ECON 452 Course Grade Calculator"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
@@ -21,35 +21,41 @@ ui <- fluidPage(
             
             h4("Input Actual or Hypothetical Grades For Each Assignment"),
             numericInput("disc",
-                        "Discussion Average (20%):",
+                        "Discussion Average (35%):",
                         min = 0,
                         max = 5,
                         step=0.1,
                         value = 4),
             helpText("Discussion average out of 5"),
-            numericInput("hw",
-                         "Homework Average (30%):",
+            numericInput("sp1",
+                         "Short Paper 1 (10%):",
                          min = 0,
                          max = 100,
                          value = 95),
-            helpText("Homework average out of 100"),
-            numericInput("midterm",
-                         "Midterm Grade (20%):",
+            numericInput("sp2",
+                         "Short Paper 2 (10%):",
                          min = 0,
                          max = 100,
                          value = 95),
-            numericInput("final",
-                         "Final Exam Grade (30%):",
+            numericInput("paper",
+                         "Term Paper (35%):",
                          min = 0,
                          max = 100,
-                         value = 95)
+                         value = 95),
+            numericInput("votes",
+                         "Tournament Votes (10%):",
+                         min = 0,
+                         max = 5,
+                         step = 0.1,
+                         value = 4),
+            helpText("Tournament vote average out of 5")
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
             h2("Your final course grade will be: "),
            h2(textOutput("printgrades"), align="center", style="color:green"),
-           p("See the", a("syllabus", href="http://ios20.classes.ryansafner.com/syllabus"), "for details. Note that I reserve the right to boost the grades of those that I believe have consistently contributed to classroom discussion, by up to 2.5%."),
+           p("See the", a("syllabus", href="http://thoughtf20.classes.ryansafner.com/syllabus"), "for details. Note that I reserve the right to boost the grades of those that I believe have consistently contributed to classroom discussion, by up to 2.5%."),
            p("Use this tool to estimate what grades on future assignments you would need to earn in order to target a specific course grade."),
            br(),
            br(),
@@ -60,8 +66,8 @@ ui <- fluidPage(
              a("Ryan Safner",
                href="http://ryansafner.com"),
              "'s", 
-             a("Industrial Organization",
-               href="http://ios20.classes.ryansafner.com"),
+             a("History of Economic Thought",
+               href="http://thoughtf20.classes.ryansafner.com"),
              "course at Hood College.")
         )
     )
@@ -71,7 +77,7 @@ ui <- fluidPage(
 server <- function(input, output) {
 
     output$printgrades <- renderText({
-        coursegrade<-round(((input$disc*4)+(input$hw*0.30)+(input$midterm*0.20)+(input$final*0.30)),4)
+        coursegrade<-round(((input$disc*7)+(input$sp1*0.10)+(input$sp2*0.10)+(input$paper*0.35)+(input$votes*2)),4)
         lettergrade<-ifelse(coursegrade>=92.5, "A",
                             ifelse(coursegrade>=89.5, "A-",
                                    ifelse(coursegrade>=86.5, "B+",
