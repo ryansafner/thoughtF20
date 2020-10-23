fd325ea68635713be872c61cda883dddba6e85d3
Ryan Safner <ryansafner@gmail.com>
2020-10-23 12:45:17 -0400

    Update 3.4-3.6 readings

--- static/slides/make-pdfs.R
+++ static/slides/make-pdfs.R
@@ -1,2 +1,2 @@
 # make-pdfs
-pagedown::chrome_print("https://thoughtf20.classes.ryansafner.com/slides/3.2-slides.html", timeout = 100)
+pagedown::chrome_print("https://thoughtf20.classes.ryansafner.com/slides/3.4-slides.html", timeout = 100)
